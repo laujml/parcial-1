62be1013018c0ba31d06b46d482a1dc5c1edc4ff
laujml <laura.an.la03@email.com>
2026-02-19 07:57:45 -0600

    Actualizacion registros

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'titulo',
+        'descripcion',
+        'isbn',
+        'copias_totales',
+        'copias_disponibles',
+        'estado',
+    ];
+
+    protected $casts = [
+        'estado' => 'boolean',
+    ];
+
+    public function loans()
+    {
+        return $this->hasMany(Loan::class);
+    }
 }
